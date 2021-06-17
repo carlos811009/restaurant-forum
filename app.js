c9f03cd8daa9d5ac7f3b7039a76f884dbe28c92e
carlos <carlos811009@gmail.com>
2021-06-17 21:52:56 +0800

    confirm user success_message & error_message

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const db = require('./models')
+const session = require("express-session")
+const flash = require('connect-flash')
 const app = express()
 const port = 3000
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash('success_message')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 
 require('./routes')(app)
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,14 +7,31 @@ const userCotroller = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    const { name, email, password } = req.body
-    User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    })
+    const { name, email, password, passwordCheck } = req.body
+    if (name === '' || email === '' || password === '') {
+      req.flash('error_messages', '每個欄位請正確填寫')
+      return res.redirect('signup')
+    }
+    if (password !== passwordCheck) {
+      req.flash('error_messages', '兩次密碼不相同')
+      return res.redirect('signup')
+    }
+    User.findOne({ where: { email: email } })
       .then(user => {
-        return res.redirect('/signin')
+        if (user) {
+          req.flash('error_messages', '此email已經註冊')
+          return res.redirect('signup')
+        }
+
+        User.create({
+          name,
+          email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+        })
+          .then(user => {
+            req.flash('success_message', '帳號建立成功')
+            return res.redirect('/signin')
+          })
       })
   }
 }
