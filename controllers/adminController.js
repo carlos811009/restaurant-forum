514f167f6e9d20bb52a5ef69806b1533b04b0054
carlos <carlos811009@gmail.com>
2021-06-18 20:55:46 +0800

    create restaurant model

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,32 @@
 const adminController = {
   getRestautants: (req, res) => {
     return res.render('admin/restaurants')
-  }
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('create')
+  },
+
+  postRestaurant: (req, res) => {
+
+  },
+  getRestaurants: (req, res) => {
+
+  },
+  getRestaurant: (req, res) => {
+
+  },
+  editRestaurant: (req, res) => {
+
+  },
+  putRestaurant: (req, res) => {
+
+  },
+  deleteRestaurant: (req, res) => {
+
+  },
+
+
 }
 
 module.exports = adminController
