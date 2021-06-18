8fa39b7f5c05e95b83ed7edeb5721c11579eff99
carlos <carlos811009@gmail.com>
2021-06-18 23:28:06 +0800

    image to restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 
 module.exports = (app, passport) => {
   const authenticated = (req, res, next) => {
@@ -26,9 +29,9 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
-  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+  app.get('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.getRestaurant)
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
-  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
 
